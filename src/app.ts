620e5d47570857ae37222aab24dbbf9594e2de2d
Woo Noh <wooyoung.noh@smartfoodnet.com>
2023-02-28 17:35:06 +0900

    feat: dotenv.config() 없을시 typescript에서 env 받아올 수 없는 상황이 존재하여 수정

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
 
 const DB_URI = process.env.MONGODB_URL || ''
 console.log("mongoDB_URI", DB_URI)
 
 mongoose.connect(DB_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  dbName: 'Users'
 });
 
 
